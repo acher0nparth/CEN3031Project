164d27454cdcf4f7e60516a776f23c95029fb817
lukefelsberg <luke.felsberg@gmail.com>
2022-04-06 18:14:41 -0400

    updated README/cleaned GUI creation

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Packaged within this repo is a 'requirements.txt' file. This file contains all o
 ## Setting up the virtual environment
 1. Run this command: `python -m venv .venv`. This will create a virtual environment called '.venv' for you to develop inside. This venv prevents any dependencies we install from being a problem globally.
 2. We now have to activate the script with this: `.venv/Scripts/Activate.ps1`.
-3. Now that the script is activated, install the dependencies like so: `pip -r requirements.txt`.
+3. Now that the script is activated, install the dependencies like so: `pip install -r requirements.txt`.
 You should be all done! 
 Many environments allow you to automatically activate the venv so you don't have to do it manually every time, maybe look into that.
--- main.py
+++ main.py
@@ -4,56 +4,55 @@ import markdown as md
 from tkhtmlview import HTMLLabel
 
 
-def showHTML():
-    # remove show button
-    show_button.pack_forget()
-    text = text_entry.get("1.0", "end")
-    html = md.markdown(text)
-    global html_area
-    global clear_button
-    html_area = HTMLLabel(window, html=html)
-    html_area.pack()
-    clear_button = tk.Button(window, text="Clear HTML", command=clearHTML)
-    clear_button.pack(side=tk.BOTTOM)
+class textEditorWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Flashify Text Editor")
+        self.geometry(
+            "%dx%d+0+0"
+            % (self.winfo_screenwidth() / 2.5, self.winfo_screenheight() - 300)
+        )
+
+        # text entry widget
+        self.text_entry = tk.scrolledtext.ScrolledText(
+            self, wrap=tk.WORD, font=("Times New Roman", 15)
+        )
+        # self.text_entry.grid(row=0,column=0)
+        self.text_entry.pack(side=tk.TOP)
+
+        # button to show HTML
+        self.show_button = tk.Button(self, text="Show HTML", command=self.showHTML)
+        self.show_button.pack(side=tk.BOTTOM)
+        self.clear_button = tk.Button(self, text="Clear HTML", command=self.clearHTML)
+
+        # adding a little menu on top
+        self.menubar = tk.Menu(self)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="Exit", command=self.quit)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.config(menu=self.menubar)
+
+    def showHTML(self):
+        # remove show button
+        self.show_button.pack_forget()
+        text = self.text_entry.get("1.0", "end")
+        html = md.markdown(text)
+        self.html_area = HTMLLabel(self, html=html)
+
+        # pack clear button first to avoid overlap
+        self.clear_button.pack(side=tk.BOTTOM)
+        self.html_area.pack()
+
+    def clearHTML(self):
+        try:
+            self.clear_button.pack_forget()
+            self.html_area.pack_forget()
+            self.show_button.pack(side=tk.BOTTOM)
+        except:
+            pass
 
 
-def clearHTML():
-    try:
-        html_area.pack_forget()
-        clear_button.pack_forget()
-        show_button.pack(side=tk.BOTTOM)
-    except:
-        pass
+window = textEditorWindow()
 
-
-# base for the GUI window
-window = tk.Tk()
-# setting what I think is an aesthetically pleasing initial dimension, user can resize from there
-w, h = window.winfo_screenwidth() / 2.5, window.winfo_screenheight() - 300
-window.geometry("%dx%d+0+0" % (w, h))
-window.title("Flashify Text Editor")
-
-# text entry widget
-text_entry = tk.scrolledtext.ScrolledText(
-    window, wrap=tk.WORD, font=("Times New Roman", 15)
-)
-text_entry.pack(side=tk.TOP)
-
-# button to show HTML
-global show_button
-show_button = tk.Button(window, text="Show HTML", command=showHTML)
-show_button.pack(side=tk.BOTTOM)
-
-# adding a little menu on top
-menubar = tk.Menu(window)
-
-filemenu = tk.Menu(menubar, tearoff=0)
-# filemenu.add_command(label="Show HTML", command = showHTML)
-# filemenu.add_command(label="Clear HTML", command = clearHTML)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=window.quit)
-
-menubar.add_cascade(label="File", menu=filemenu)
-
-window.config(menu=menubar)
 window.mainloop()
