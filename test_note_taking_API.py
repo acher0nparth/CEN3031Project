991fb1126a179763b5458f368f06915e33b24905
Blake Thacker <62034641+acher0nparth@users.noreply.github.com>
2022-04-07 16:01:24 -0400

    black reformatting for sanity

--- test_note_taking_API.py
+++ test_note_taking_API.py
@@ -1,10 +1,15 @@
 import sys
- 
-sys.path.append('./CEN3031Project')
+
+sys.path.append("./CEN3031Project")
 from note_taking_API import *
+
 
 def test_db_create_db():
     assert db_create_db(connection) == "sprint1_rev_db"
 
+
 def test_db_get_note():
-    assert db_get_note(connection, 3) == ('Note 1 updated', 'testing updating my first note')
+    assert db_get_note(connection, 3) == (
+        "Note 1 updated",
+        "testing updating my first note",
+    )
