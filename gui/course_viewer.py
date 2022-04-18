7728e93fb7a891b90a2843b645f1aa25c69952fd
Blake Thacker <62034641+acher0nparth@users.noreply.github.com>
2022-04-18 00:35:44 -0400

    naming conventions

--- gui/course_viewer.py
+++ gui/course_viewer.py
@@ -49,21 +49,21 @@ class course_viewer(tk.Tk):
         self.frame.grid(row=3, column=1, columnspan=2, pady=5)
         self.scrollbar = tk.Scrollbar(self.frame, orient=tk.VERTICAL)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.list_box = tk.Listbox(
+        self.notes_list_box = tk.Listbox(
             self.frame,
             width=50,
             yscrollcommand=self.scrollbar.set,
             font=("Courier New", 10),
         )
-        self.list_box.pack(expand=True, fill=tk.Y)
-        self.scrollbar.config(command=self.list_box.yview)
+        self.notes_list_box.pack(expand=True, fill=tk.Y)
+        self.scrollbar.config(command=self.notes_list_box.yview)
 
-        # populate list_box with notes from specific course in the database:
+        # populate notes_list_box with notes from specific course in the database:
         # not sure how this works quite yet
         nts = db_get_all_course_note_titles(self.course)
         if nts:
             for nt in nts:
-                self.list_box.insert(tk.END, nt[0])
+                self.notes_list_box.insert(tk.END, nt[0])
 
         self.view_note_button = tk.Button(
             self, text="View Note", height=1, width=15, command=self.view_note
@@ -129,13 +129,13 @@ class course_viewer(tk.Tk):
 
     def view_note(self):
         # will create blank page if no note is selected
-        note = self.list_box.get(tk.ANCHOR)
+        note = self.notes_list_box.get(tk.ANCHOR)
         self.destroy()
         self = nv.note_viewer(self.course, note)
 
     def edit_note(self):
         # will create blank page if no note is selected
-        note = self.list_box.get(tk.ANCHOR)
+        note = self.notes_list_box.get(tk.ANCHOR)
         self.destroy()
         self = te.text_editor(self.course, note)
 
@@ -165,9 +165,9 @@ class course_viewer(tk.Tk):
         cancel_button.pack(side=tk.RIGHT)
 
     def delete_note(self):
-        note_title = self.list_box.get(tk.ANCHOR)
+        note_title = self.notes_list_box.get(tk.ANCHOR)
         db_delete_note(note_title, self.course)
-        self.list_box.delete(tk.ANCHOR)
+        self.notes_list_box.delete(tk.ANCHOR)
 
     def add_to_list(self, note_title):
         """add specified note to note list"""
@@ -176,7 +176,7 @@ class course_viewer(tk.Tk):
         db_insert(note_title, self.course, "")
 
         # this is necessary to show newly added notes while still in window:
-        self.list_box.insert(tk.END, self.note_name.get(1.0, tk.END))
+        self.notes_list_box.insert(tk.END, self.note_name.get(1.0, tk.END))
 
         self.popup.destroy()
 
